2b7d9f8ce6c8a957068e94ab56a5b8c5ea36ed4c
shixin <783035017@qq.com>
2021-01-12 15:08:12 +0800

    弹窗的添加

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,15 +5,10 @@
         <h1>{{ msg }}</h1>
         <h2>Essential Links</h2>
         <el-button v-on:click="press" class="button-type">主页</el-button>
-
         <el-button v-on:click="press1" class="button-type">登录页面</el-button>
-
-        <el-button v-on:click="press2" type="danger" plain class="button-type"
-          >task页面</el-button
-        >
-        <el-button v-on:click="press3" type="info" plain class="button-type"
-          >主页面</el-button
-        >
+        <el-button v-on:click="press2" type="danger" class="button-type">task页面</el-button>
+        <el-button v-on:click="press3" type="info" class="button-type">主页面</el-button>
+        <el-button v-on:click="open" type="info" class="button-type">弹窗</el-button>
       </el-col>
     </div>
   </div>
@@ -25,7 +20,7 @@ export default {
   data() {
     return {
       msg: "Welcome to Your Vue.js App",
-      title: "you are so great",
+      title: "you are so great"
     };
   },
   methods: {
@@ -41,7 +36,18 @@ export default {
     press3() {
       this.$router.push("/home");
     },
-  },
+    open() {
+      this.$alert("这是一段内容", "标题名称", {
+        confirmButtonText: "确定",
+        callback: action => {
+          this.$message({
+            type: "info",
+            message: `action: ${action}`
+          });
+        }
+      });
+    }
+  }
 };
 </script>
 

8da21cbaf913f8b17c7c6c38f6382951d00f2ddb
shixin <783035017@qq.com>
2021-01-20 14:29:53 +0800

    伪元素联系

--- src/views/fish/home/FishPond.vue
+++ src/views/fish/home/FishPond.vue
@@ -1,14 +1,78 @@
 <template>
   <div class="container">
-    <span>我的页面</span>
+    <div class="tag">
+      <span class="s">2020年5月21日 四月廿九 星期四</span>
+    </div>
+    <div class="tip">
+      <div class="left">养殖指导</div>
+      <div class="right">养殖指导</div>
+    </div>
+    <div class="tag">
+      <span class="s">我的鱼塘</span>
+    </div>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  data(){
+    return {
+
+    }
+  },
+  methods:{
+
+  }
+};
 </script>
 <style lang="less" scoped>
 .container {
   display: flex;
   flex-direction: column;
+  overflow: hidden;
+  .tag {
+    width: 100vw;
+    height: 53px;
+    display: flex;
+    flex-direction: row;
+    clear: both;
+    overflow: hidden;
+    align-items: center;
+    .s {
+      padding: 0;
+      margin: 0;
+      text-align: center;
+      font-size: 20px;
+      line-height: 20px;
+      color: black;
+    }
+    &::before {
+      content: "";
+      width: 5px;
+      height: 16px;
+      background-color: #4d70ea;
+      display: block;
+      margin-right: 10px;
+      margin-left: 20px;
+    }
+  }
+  .tip {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    .left {
+      flex: 1;
+      height: 72px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+    .right {
+      flex: 1;
+      height: 72px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+  }
 }
 </style>

616cfef7280e5ba99b956115bf7493cdd0093a6c
shixin <783035017@qq.com>
2021-01-22 10:22:21 +0800

    异步

---
+++ src/views/promise/SyncFile.vue
@@ -0,0 +1,25 @@
+<template>
+  <diV>
+    <el-button type="primary" size="default" @click="t1"></el-button>
+    <el-button type="text" size="default" @click="t1"></el-button>
+  </diV>
+</template>
+<script>
+function timeout(ms) {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, ms, "done");
+  });
+}
+
+export default {
+  methods: {
+    t1() {
+      timeout(100).then(value => {
+        console.log(value);
+      });
+    }
+  }
+};
+</script>
+<style scoped>
+</style>

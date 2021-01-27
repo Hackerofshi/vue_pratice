32e4d081923c2134775fef042e8ab31804ba1eac
shixin <783035017@qq.com>
2021-01-27 13:54:35 +0800

    日常提交

---
+++ src/views/demo/class/index.vue
@@ -0,0 +1,27 @@
+<template></template>
+<script>
+function Point(x, y) {
+  this.x = x;
+  this.y = y;
+}
+
+Point.prototype.toString = function() {
+  return "(" + this, x + "," + this.y + ")";
+};
+
+var p = new Point(1, 2);
+
+//可以写成这个样子
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  toString() {
+    return "(" + this.x + "," + this.y + ")";
+  }
+}
+
+
+
+</script>
